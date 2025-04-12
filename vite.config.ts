dcd544e330665f488668aee1442cdbd922ffb3a7
Himansa Eshan <41321171+himansaE@users.noreply.github.com>
2025-04-12 12:56:55 +0530

    Refactor app to integrate Clerk authentication and replace CSS with Tailwind
    - Removed existing CSS styles and imported Tailwind CSS.
    - Updated App component to include Clerk's SignedIn and SignedOut components for user authentication.
    - Removed index.css as styles are now managed by Tailwind.
    - Wrapped App component in ClerkProvider in main.tsx for authentication context.
    - Configured Vite to use Tailwind CSS and Google Fonts for styling.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import Unfonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      google: {
+        families: [
+          "Ruslan Display",
+          {
+            name: "Poppins",
+            styles: "wght@400;600",
+            defer: true,
+          },
+        ],
+      },
+    }),
+  ],
+});
